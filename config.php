799aeaa99a5c53f434354efc059eec1b8da9e248
PNP\jermino <jermino6@gmail.com>
2017-06-01 10:42:11 +0800

    added config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3306');
+   define('DB_USERNAME', 'id1825404_jermino');
+   define('DB_PASSWORD', 'Ermino1997');
+   define('DB_DATABASE', 'id1825404_teammaster');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
